995f5d172f8841cf27c91273fc08ee1ff0815aad
Whose dream is this? <whosedreamisthis@gmail.com>
2021-12-17 13:41:02 -0800

    tweaks

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Rank = () => {
     return (
         <div>
-            <div className='white f3'>
+            <div className='darkest-gray b f3'>
                 { 'Andrei your current rank is ...'}
             </div>
-            <div className='white f1'>
+            <div className='darkest-gray f1'>
                 { '#5'}
             </div>
         </div>
--- src/components/imagelinkform/ImageLinkForm.js
+++ src/components/imagelinkform/ImageLinkForm.js
@@ -4,7 +4,7 @@ import './ImageLinkForm.css';
 const ImageLinkForm = () => {
         return (
             <div>
-                <p className='f3'>
+                <p className='f3 white'>
                     {
                         "This magic brain will detect faces in your pictures. Give it a try."
                     }
@@ -12,7 +12,7 @@ const ImageLinkForm = () => {
                 <div className="center">
                     <div className="form center pa4 br3 shadow-5">
                         <input className = "f4 pa3 w-70 center" type='tex' />
-                        <button className = "w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
+                        <button className = "w-30 grow f4 link ph3 pv2 dib dark-blue bg-light-green">Detect</button>
                     </div>
                 </div>
             </div>
