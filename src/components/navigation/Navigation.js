46190ef1484d0c161a6ceab79b4687d66ec9015d
Whose dream is this? <whosedreamisthis@gmail.com>
2021-12-17 12:20:47 -0800

    logo and navigation components

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'tachyons';
+
+const Navigation = () => {
+        return (
+            <nav style={{display:'flex',justifyContent:'flex-end'}}>
+            <a href="#" className='f3 link black underline-hover dim pa3 pointer' >Sign Out</a> 
+            </nav>
+        );
+}
+
+export default Navigation;
